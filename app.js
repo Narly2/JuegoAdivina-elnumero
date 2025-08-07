afd7e599e667d3a26f2df3314f691df951eda45c
Narly2 <111981225+Narly2@users.noreply.github.com>
2025-08-06 21:31:45 -0500

    code of app

---
+++ app.js
@@ -0,0 +1,22 @@
+generarNumeroSecreto ();
+
+
+function asignarTextoElemento(elemento,texto){
+    let elementoHTML=document.querySelector(elemento);
+        elementoHTML.innerHTML=texto;
+        return;
+}
+
+function verificarIntento(){
+    let numeroDeUsuario= document.getElementById('valorUsuario').Value;
+
+    alert('Hello');
+    return;
+}
+
+function generarNumeroSecreto(){
+    return Math.floor(Math.random()*10)+1;
+}
+
+asignarTextoElemento(h1, "Juego del número secreto!");
+asignarTextoElemento(p, "Indica un número entre 1 y 10");
